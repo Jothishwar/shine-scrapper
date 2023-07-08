0231d07b94b0b8743e8ce903bb7482e1048fef46
jothishwar <jothishwarvvr@gmail.com>
2023-07-08 23:37:40 +0530

    connecting database

--- shine.py
+++ shine.py
@@ -7,6 +7,12 @@ from email.message import EmailMessage
 import psycopg2
 from psycopg2.extras import execute_values
 
+USERNAME = "talentaijobsuser"
+PASSWORD = "jkrowlingtalentai12?"
+DB_NAME = "talentai_jobs_db"
+URL = "talentai-jobs-db.c95l3n9ofmp6.ap-south-1.rds.amazonaws.com"
+PORT = 5432
+
 payload = {}
 headers = {
   'authority': 'www.shine.com',
@@ -34,6 +40,31 @@ for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
     # print(resp_json['data'][0][0])
 print('total categories - ',len(category_names))
 total=0
+
+def send_email(subject,body):
+    # Set up SMTP server details
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    sender_email = 'jothishwarvvr@gmail.com'  # Your email address
+    sender_password = 'nowfywvjxxwwfyux'  # Your email password
+    recipient_email = 'jothishwarvvr@gmail.com'  # Recipient's email address
+
+    # Create the email message
+    message = EmailMessage()
+    message['From'] = sender_email
+    message['To'] = recipient_email
+    message['Subject'] = subject
+    message.set_content(body)
+    
+    # Send the email
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.send_message(message)
+        print('Email sent successfully!')
+    except smtplib.SMTPException as e:
+        print('Error sending email:', str(e))
 
 def get_qualification(desc):
     qualification = re.search(r'\b(Education|QUALIFICATION)\s*[:\- ]*\s*(\w[^,\n]*)', desc)
@@ -77,7 +108,28 @@ def get_sal(job_soup,desc):
             return salary.group(1).strip()
         else:
             return None
-
+def format_sal(sal):
+    if sal==None:
+        return None
+    fsal=[]
+    matches=re.findall(r"\d+",sal)
+    if len(matches)>0:
+        if len(matches)==1:
+            if int(matches[0])/1000>=1 and str(matches[0]).count('0')==3:
+                pass
+            fsal.append(int(matches[0]))
+            fsal.append(None)
+        if len(matches) >= 2:
+            if(int(matches[0])!=int(matches[1])):
+                fsal.append(int(matches[0]))
+                fsal.append(int(matches[1]))
+            else:
+                fsal.append(int(matches[0]))
+                fsal.append(None)
+    else:
+        fsal.append(None)
+        fsal.append(None)
+    return sal
 for cat in category_names:
     fetched=0
     job_list=[]
@@ -88,7 +140,20 @@ for cat in category_names:
     c=category_json['count']
     print('available jobs - ',c)
     total+=c
+    #----------------------Connecting to Postgresql Database-----------------------------------------------
+    conn = psycopg2.connect(database=DB_NAME, password=PASSWORD, user=USERNAME, host=URL, port=PORT)
+    cur = conn.cursor()
+    #-----------------Fetching existing job links to remove duplicates-------------------------------------
+    try:
+        cur.execute("select url from jobs where url like '%timesjobs%' and category='"+str(c)+"'")
+        rec=cur.fetchall()
+        existing_links=list([j[0] for j in rec])
+    except:
+        existing_links=[]
+
     for page in range(1,category_json['num_pages']+1):
+        print('page - ',page)
+        #---------------Looping through the pages to scrape all the jobs in that category--------------------------
         time.sleep(randint(2,5))
         cat_api_url=f"https://www.shine.com/api/v2/search/simple/?q={cat}&page={page}&sort=1"
         resp = requests.request("GET", cat_api_url, headers=headers, data=payload)
@@ -97,22 +162,30 @@ for cat in category_names:
         try:
             n=len(job_json['results'])
             fetched+=n
+            #-----------------looping through all the jobs in each page---------------------------------
             for job in range(n):
+                print('j-',job)
+                da=job_json['results'][job]['jPDate'][:10]
+                link="https://www.shine.com/jobs/"+job_json['results'][job]['jSlug']
+                if datetime.strptime(da,'%Y-%m-%d').date() < date.today()-timedelta(days=60):
+                    continue
+                if link in existing_links:
+                    continue
                 titl=job_json['results'][job]['jJT']
                 cmp=job_json['results'][job]['jCName']
                 desc=format_desc(job_json['results'][job]['jJD'])
                 loc="/".join(job_json['results'][job]['jLoc'])
-                da=job_json['results'][job]['jPDate'][:10]
                 sk=job_json['results'][job]['jKwd']
                 exp=job_json['results'][job]['jExp']
                 minmax=format_exp(exp)
-                link="https://www.shine.com/jobs/"+job_json['results'][job]['jSlug']
                 qua=get_qualification(desc)
 
-                #salary and employment type details are extracted using bs4 module
+                #--------------salary and employment type details are extracted using bs4 module--------------------
                 job_resp=requests.get(link)
                 job_soup=BeautifulSoup(job_resp.content,'html.parser')
                 sal=get_sal(job_soup,desc)
+                print(sal)
+                fsal=format_sal(sal)
                 try:
                     job_html=job_soup.select_one('script[type="application/ld+json"]')
                     job_json1=json.loads(job_html.text.strip())
@@ -120,12 +193,22 @@ for cat in category_names:
                     print('Failed to fetch json for emp type')
                     print(link,' - ',e)
                 ty=job_json1['employmentType']
-                print((titl,cat,loc,da,link,desc,sk,ty,cmp,minmax[0],minmax[1],qua,None,sal))
+                # print((titl,cat,loc,da,link,desc,sk,ty,cmp,minmax[0],minmax[1],qua,None,sal))
+                job_list.append((titl,cat,loc,da,link,desc,sk,ty,cmp,minmax[0],minmax[1],qua,None,sal))
         except Exception as e:
             print(e)
             print('\nfailed to fetch page')
             print('category - ',cat,'\t page number - ',page,'\n')
             continue
     print('jobs fetched using api - ',fetched)
+    if len(job_list)>0:
+        execute_values(cur,
+            '''INSERT INTO jobs(title,category,location,date_posted,url,description,skills,job_type,company_name,minimum_experience,maximum_experience,qualifications,company_logo_url,salary)
+            VALUES %s''',
+            job_list)
+        conn.commit()
+        conn.close()
+        print(cat,' Database updated - ',len(job_list))
+        send_email('Shine Status',f'Shine category {cat} completed ssuccessfully \n Database updated - '+str(len(job_list)))
 print('Overall jobs fetched - ',total)
-print('Success !!!')
+send_email('Timesjobs Status','Timesjobs scraping completed successfully')
